8af5132431dc4c4dc939c574ab6bc2639cfc50f4
frankgrimm@gmail.com <dev@speicher.ghb.fh-furtwangen.de>
2010-12-05 13:51:21 +0100

    Use standard error handling and escalate over the error event.

--- examples/configfiles.js
+++ examples/configfiles.js
@@ -14,6 +14,10 @@ var sampleDefaults = {rootkey:'rootValue',
 
 var config = new ConfigParser(sampleDefaults);
 
+config.on('error', function(err) {
+  console.log("[ERRROR:config] " + err.message);
+});
+
 // write defaults
 config.write(require('fs').createWriteStream('defaults.cfg'));
 config.on('written', function() {
@@ -25,8 +29,13 @@ readConfig.on('readfile', function(filename) {
   console.log("File <" + filename + "> read");
 
   readConfig.write(process.stdout, '\n', true);
+}).on('error', function(err) {
+  console.log("[ERROR:readConfig] " + err.message);
 });
 
 });
 
-
+process.on('uncaughtException', function(err) {
+  console.log(err.message);
+  console.log(err.stack);
+});
